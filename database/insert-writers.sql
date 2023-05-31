0a8c721ab5b8fb8b169d48fc0eddf9d75cee907f
Carmen Barcia <carmen.barcia@hotmail.com>
2023-05-31 15:16:02 +0200

    dded the tables for star and writer and the data from both

---
+++ database/insert-writers.sql
@@ -0,0 +1,11 @@
+INSERT INTO writers (name, writer_email)
+VALUES ('Arthur C Clarke', 'arthur@clarke.com'),
+('George Lucas', 'george@email.com'),
+('Harper Lee', 'harper@lee.com'),
+('James Cameron', 'james@cameron.com'),
+('Boris Pasternak', 'boris@boris.com'),
+('Frederick Frank', 'fred@frank.com'),
+('Theodoros Angelopoulos', 'theo@angelopoulos.com'),
+('Erik Hazelhoff Roelfzema', 'erik@roelfzema.com'),
+('Krzysztof Kieślowski', 'email@email.com'),
+('Edmond Rostand', 'edmond@rostand.com');
