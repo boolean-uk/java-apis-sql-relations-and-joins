638c7fc752640ee5665a327ebb7b0797a5ce9592
Nikolaos Giagkinis <giagkinis.nick@gmail.com>
2023-05-31 16:59:41 +0300

    First 3 core queries

--- database/data-manipulation-queries.sql
+++ database/data-manipulation-queries.sql
@@ -1,9 +1,14 @@
 -- Show the title and director name for all films
-SELECT title,directors.name FROM movies, directors WHERE movies.director_id = directors.id
+SELECT movies.title,directors.name FROM movies JOIN directors ON movies.director_id = directors.id;
 
 -- Show the title, director and star name for all films
+SELECT movies.title,directors.name,star.name from movies as m
+    JOIN directors as d ON m.director_id = d.id
+    JOIN star as s ON m.star_id = s.id;
 
 -- Show the title of films where the director is from the USA
+SELECT movies.title FROM movies, directors
+	WHERE movies.director_id = directors.id AND directors.country = 'USA';
 
 -- Show only those films where the writer and the director are the same person
 
--- database/db-initialization/insert-directors.sql
+++ database/db-initialization/insert-directors.sql
@@ -2,7 +2,7 @@ INSERT INTO directors (name , country)
 VALUES  ('Stanley Kubrick','USA'),
         ('George Lucas','USA'),
         ('Robert Mulligan','USA'),
-        ('James Cameron','USA'),
+        ('James Cameron','Canada'),
         ('David Lean','UK'),
         ('Anthony Mann','USA'),
         ('Theodoros Angelopoulos','Greece'),
