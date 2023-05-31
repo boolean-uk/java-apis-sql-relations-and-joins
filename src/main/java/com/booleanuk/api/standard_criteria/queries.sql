e5fe39c444db74a0173b85af1d03647f6996247f
Rose Breedveld <rosebreedveld@hotmail.com>
2023-05-31 09:55:05 -0400

    committed core

--- src/main/java/com/booleanuk/api/standard_criteria/queries.sql
+++ src/main/java/com/booleanuk/api/standard_criteria/queries.sql
@@ -29,15 +29,41 @@ where
 -- Make at least 5 more queries to demonstrate your understanding of joins, and other relationships between tables.
 
 -- Show directors and film titles for films where the director is from USA and films have a score of 8 or higher
-
+select director_name, title
+from films, directors
+where
+    films.director_id = directors.id
+    and
+    directors.country = 'USA'
+    and
+    films.score >= 8;
 
 -- Show stars and film titles for films where the star is older than 50 years old
-
+select title, star_name
+from films, stars
+where
+stars.id = films.star_id
+and
+date_of_birth <= '1973-05-31';
 
 -- Show the email of the writers that are also directors
+select email
+from
+writers, directors
+where
+directors.director_name = writers.writer_name;
 
-
--- Show stars and film titles where the film is released at least 30 years after the star was born 
+-- count the number of movies for each country where the directors are coming from?
+select country, count(*)
+from directors, films
+where films.director_id = directors.id
+group by directors.country;
 
 
 -- Show stars for the films that have a score of 9
+select title, star_name
+from films, stars
+where
+    films.star_id = stars.id
+    and
+    films.score = 9;
