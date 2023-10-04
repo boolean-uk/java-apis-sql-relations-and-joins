db62546cdfad53e1c6558b262da4a1883ccca5da
Ali Keskin <cmdalikeskin@gmail.com>
2023-10-04 21:58:53 +0200

    Fixed the Core part of the exercise

--- src/Core exercises(TablePlus codes).sql
+++ src/Core exercises(TablePlus codes).sql
@@ -1,12 +1,3 @@
---Create the film table
-CREATE TABLE IF NOT EXISTS film (
-    film_id SERIAL PRIMARY KEY,
-    title TEXT,
-    year INTEGER,
-    genre TEXT,
-    score INTEGER
-);
-
 --Create the director table
 CREATE TABLE IF NOT EXISTS director (
     director_id SERIAL PRIMARY KEY,
@@ -23,6 +14,7 @@ CREATE TABLE IF NOT EXISTS writer (
 
 );
 
+
 --Create the star table
 CREATE TABLE IF NOT EXISTS star (
     star_id SERIAL PRIMARY KEY,
@@ -31,20 +23,33 @@ CREATE TABLE IF NOT EXISTS star (
 
 );
 
---Insert the values for the film table
-INSERT INTO film
-(title,year,genre,score)
-VALUES
-	('2001: A Space Odyssey',1968,'ScienceFiction',10),
-	('Star Wars:A NewHope',1977,'ScienceFiction',7),
-	('To Kill A Mockingbird',1962,'Drama',10),
-	('Titanic',1997,'Romance',5),
-	('Dr Zhivago',1965,'Historical',8),
-	('El Cid',1961,'Historical',6),
-	('Voyage to Cythera',1984,'Drama',8),
-	('Soldier of Orange',1977,'Thriller',8)
-	('ThreeColours:Blue',1993,'Drama',8),
-	('Cyrano de Bergerac',1990,'Historical',9);
+ALTER TABLE star
+RENAME COLUMN name TO star;
+
+
+--Create the film table
+CREATE TABLE IF NOT EXISTS film (
+    film_id SERIAL PRIMARY KEY,
+    title TEXT,
+    year INTEGER,
+    genre TEXT,
+    score INTEGER,
+    director_id INTEGER,
+    writer_id INTEGER,
+    star_id INTEGER,
+    CONSTRAINT fk_director_id
+    	FOREIGN KEY (director_id)
+    		REFERENCES director(director_id),
+    CONSTRAINT fk_writer_id
+    	FOREIGN KEY (writer_id)
+    		REFERENCES writer(writer_id),
+    CONSTRAINT fk_star_id
+    	FOREIGN KEY (star_id)
+    		REFERENCES star(star_id)
+);
+
+
+
 
 --Insert the values for the director table
 INSERT INTO director (director,country)
@@ -75,7 +80,7 @@ VALUES
     ('Edmond Rostand', 'edmond@rostand.com');
 
 --Insert the values star table
-INSERT INTO star(star dob)
+INSERT INTO star(star, dob)
 VALUES
     ('Keir Dullea', '30/05/1936'),
     ('Mark Hamill', '25/09/1951'),
@@ -88,6 +93,21 @@ VALUES
     ('Juliette Binoche', '09/03/1964'),
     ('Gerard Depardieu', '27/12/1948');
 
+--Insert the values for the film table
+INSERT INTO film
+(title, year, genre, score ,director_id, writer_id, star_id)
+VALUES
+	('2001: A Space Odyssey',1968,'ScienceFiction',10,1,1,1),
+	('Star Wars:A NewHope',1977,'ScienceFiction',7,2,2,2),
+	('To Kill A Mockingbird',1962,'Drama',10,3,3,3),
+	('Titanic',1997,'Romance',5,4,4,4),
+	('Dr Zhivago',1965,'Historical',8,5,5,5),
+	('El Cid',1961,'Historical',6,6,6,6),
+	('Voyage to Cythera',1984,'Drama',8,7,7,7),
+	('Soldier of Orange',1977,'Thriller',8,8,8,8),
+	('ThreeColours:Blue',1993,'Drama',8,9,9,9),
+	('Cyrano de Bergerac',1990,'Historical',9,10,10,10);
+
 
 SELECT*FROM writer;
 
@@ -98,34 +118,34 @@ SELECT*FROM writer;
 ---------------------
 
 --1:
-SELECT title, director
-FROM film
-  JOIN director
-    ON film_id = director_id;
+SELECT f.title, d.director
+FROM film AS f
+  JOIN director AS d
+    ON f.director_id = d.director_id;
 
 --2:
-SELECT fi.title, dir.director, str.star
-FROM film AS fi
-LEFT JOIN director as dir on fi.film_id = dir.director_id
-LEFT JOIN star as str on dir.director_id = str.star_id;
+SELECT f.title, dir.director, str.star
+FROM film AS f
+LEFT JOIN director as dir on f.director_id = dir.director_id
+LEFT JOIN star as str on f.star_id = str.star_id;
 
 --3:
 SELECT fi.title
 FROM film AS fi
-LEFT JOIN director as dir on fi.film_id = dir.director_id
+LEFT JOIN director as dir on fi.director_id = dir.director_id
 WHERE dir.country ='USA';
 
 --4:
 SELECT fi.title
 FROM film AS fi
-LEFT JOIN director as dir on fi.film_id = dir.director_id
-LEFT JOIN writer as wri on fi.film_id = wri.writer_id
+LEFT JOIN director as dir on fi.director_id = dir.director_id
+LEFT JOIN writer as wri on fi.writer_id = wri.writer_id
 WHERE wri.writer = dir.director;
 
 --5:
 SELECT dir.director, fi.title
 FROM film AS fi
-LEFT JOIN director as dir on fi.film_id = dir.director_id
+LEFT JOIN director as dir on fi.director_id = dir.director_id
 WHERE fi.score >= 8;
 
 --6:
@@ -134,33 +154,34 @@ WHERE fi.score >= 8;
 --EXTRA QUERY 1:
 SELECT
 director.director AS directorName, film.title
-
 FROM film
-LEFT JOIN director  on film.film_id = director.director_id
+LEFT JOIN director  on film.director_id = director.director_id
 	WHERE
 	director.director = 'Stanley Kubrick';
 
 --EXTRA QUERY 2:
 SELECT
-    film_id,
-    film.title,
-    director.director,
-    director.country,
-    star.star,
-    star.dob,
-    writer.writer,
-    writer.email,
-    film.year,
-    film.genre,
-    film.score
+    f.film_id AS "Id",
+    f.title AS "Title",
+    d.director AS "Director",
+    d.country AS "Director Country",
+    s.star AS "Star DOB",
+    s.dob AS "Star DOB",
+    w.writer AS "Writer",
+    w.email AS "Writer Email",
+    f.year AS "Year",
+    f.genre AS "Genre",
+    f.score AS "Score"
 FROM
-    film
+    film AS f
 JOIN
-    director ON film_id = director_id
+director AS d ON f.director_id = d.director_id
 JOIN
-    star ON film_id = star_id
+writer AS w ON f.writer_id = w.writer_id
 JOIN
-    writer ON film_id = writer_id;
+star AS s ON f.star_id = s.star_id;
+
+
 
 --EXTRA QUERY 3:
 SELECT
@@ -173,9 +194,9 @@ SELECT
 FROM
     film
 JOIN
-    star ON film_id = star_id
+    star ON film.star_id = star.star_id
 JOIN
-    writer ON film_id = writer_id
+    writer ON writer.writer_id= film.writer_id
 	WHERE
 		film.year <2000;
 
@@ -190,11 +211,11 @@ SELECT
 FROM
     film
 JOIN
-    director ON film_id = director_id
+    director ON film.director_id = director.director_id
 JOIN
-    star ON film_id = star_id
+    star ON star.star_id= film.star_id
 JOIN
-    writer ON film_id = writer_id
+    writer ON film.writer_id = writer.writer_id
     ORDER BY
     	star.star ASC;
 
@@ -211,11 +232,11 @@ SELECT
 FROM
     film
 JOIN
-    director ON film_id = director_id
+    director ON film.director_id = director.director_id
 JOIN
-    star ON film_id = star_id
+    star ON star.star_id = film.star_id
 JOIN
-    writer ON film_id = writer_id
+    writer ON writer.writer_id = film.writer_id
     WHERE
     	director.country != 'USA'
     AND
@@ -223,7 +244,3 @@ JOIN
 	ORDER BY
 		film.title DESC
     ;
-
-
-
-
